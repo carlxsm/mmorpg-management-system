9a988d804b86c7c6a47b708d7e8b4ab5f2beb15c
Carlos Mendes <58920321+CarlosMendes2@users.noreply.github.com>
2024-05-31 21:57:38 -0300

    Spring initializer

---
+++ data-crud/src/main/java/com/hallucinationlab/mmorpgdatamanagement/datacrud/DataCrudApplication.java
@@ -0,0 +1,13 @@
+package com.hallucinationlab.mmorpgdatamanagement.datacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCrudApplication.class, args);
+	}
+
+}
---
+++ data-crud/src/test/java/com/hallucinationlab/mmorpgdatamanagement/datacrud/DataCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallucinationlab.mmorpgdatamanagement.datacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
